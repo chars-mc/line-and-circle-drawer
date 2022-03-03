728bad3d528ed66abb19c64532b7f01c3e01327c
Carlos Manuel <chars.dev@gmail.com>
2022-03-03 09:52:24 -0600

    Ad points per quadrant to draw

--- utils/vertex.go
+++ utils/vertex.go
@@ -2,14 +2,16 @@ package utils
 
 import "github.com/chars-mc/opengl-exercises/domain"
 
+const pointsPerQuadrant = 100
+
 func GetPointsFromCoordinates(coordinates domain.Coordinates) []float32 {
 	var points []float32
 
 	for _, coordinate := range coordinates {
 		point := []float32{
-			float32(coordinate.X), // X axis
-			float32(coordinate.Y), // Y axis
-			0.0,                   // Z axis
+			float32(coordinate.X) / pointsPerQuadrant, // X axis
+			float32(coordinate.Y) / pointsPerQuadrant, // Y axis
+			0.0, // Z axis
 		}
 		points = append(points, point...)
 	}
