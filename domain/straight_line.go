6045f781c37ddfa309af5515f8f208c39eecbf7a
Carlos Manuel <chars.dev@gmail.com>
2022-03-02 19:07:53 -0600

    Implement algorithm

--- domain/straight_line.go
+++ domain/straight_line.go
@@ -1,5 +1,7 @@
 package domain
 
+import "math"
+
 // StraightLine represents a line from coordinates a to b
 type StraightLine struct {
 	a Coordinate
@@ -16,7 +18,20 @@ func NewStraightLine(a, b Coordinate) *StraightLine {
 func (line *StraightLine) GetCoordinates() Coordinates {
 	var coordinates Coordinates
 
-	// TODO: implement algorithm
+	m := float64(line.b.Y-line.a.Y) / float64(line.b.X-line.a.X) // get the slope of the line
+	b := float64(line.a.Y) - m*float64(line.a.X)                 // find b
+	b = math.Round(b*10) / 10                                    // round b to one decimals
+	k := line.b.X - line.a.X                                     // get number of iterations
+
+	for i := 0; i < k; i++ {
+		y := m*float64(line.a.X) + b // yk = mx + b
+
+		coordinates = append(coordinates, Coordinate{
+			X: line.a.X,
+			Y: int(math.Round(y)),
+		})
+		line.a.X++
+	}
 
 	return coordinates
 }
--- domain/straight_line_test.go
+++ domain/straight_line_test.go
@@ -11,7 +11,28 @@ func TestStraightLine_GetCoordinates(t *testing.T) {
 		l        *StraightLine
 		expected Coordinates
 	}{
-		// TODO: add test cases
+		{
+			name: "A(2, 2) and B(7, 8) - slope less than 1",
+			l:    NewStraightLine(Coordinate{2, 2}, Coordinate{7, 8}),
+			expected: Coordinates{
+				{2, 2}, {3, 3}, {4, 4}, {5, 6}, {6, 7},
+			},
+		},
+		{
+			name: "A(2, 2) and B(8, 2) - slope 0 (0 deg)",
+			l:    NewStraightLine(Coordinate{2, 2}, Coordinate{8, 2}),
+			expected: Coordinates{
+				{2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}, {7, 2},
+			},
+		},
+		{
+			name: "A(2, 2) and B(9, 9) - slope 1 (45 deg)",
+			l:    NewStraightLine(Coordinate{2, 2}, Coordinate{9, 9}),
+			expected: Coordinates{
+				{2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8},
+			},
+		},
+		// TODO: add undefined slope case
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
