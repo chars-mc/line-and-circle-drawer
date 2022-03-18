4b23279aa1fa35eb18abff9b742a8881ff7fbb88
Carlos Manuel <chars.dev@gmail.com>
2022-03-18 14:15:50 -0600

    Implement circumference algorithm

--- domain/circle.go
+++ domain/circle.go
@@ -14,8 +14,52 @@ func NewCircle(center Coordinate, radio int) *Circle {
 
 func (c *Circle) GetCoordinates() Coordinates {
 	var coordinates Coordinates
+	initialCoordinates := c.getInitialCoordinates()
 
-	// TODO: implement algorithm
+	for _, point := range initialCoordinates {
+
+		coors := Coordinates{
+			// quadrant 1
+			Coordinate{X: point.X + c.Center.X, Y: point.Y + c.Center.Y},
+			Coordinate{X: point.Y + c.Center.X, Y: point.X + c.Center.Y},
+			// quadrant 2
+			Coordinate{X: point.X + c.Center.X, Y: -point.Y + c.Center.Y},
+			Coordinate{X: point.Y + c.Center.X, Y: -point.X + c.Center.Y},
+			// quadrant 3
+			Coordinate{X: -point.X + c.Center.X, Y: -point.Y + c.Center.Y},
+			Coordinate{X: -point.Y + c.Center.X, Y: -point.X + c.Center.Y},
+			// quadrant 4
+			Coordinate{X: -point.X + c.Center.X, Y: point.Y + c.Center.Y},
+			Coordinate{X: -point.Y + c.Center.X, Y: point.X + c.Center.Y},
+		}
+		coordinates = append(coordinates, coors...)
+	}
+
+	return coordinates
+}
+
+// getInitialCoordinates calculates the initial coordinates
+func (c *Circle) getInitialCoordinates() Coordinates {
+	var coordinates Coordinates
+
+	point := Coordinate{0, c.Radio} // first coordinate
+	p := 5/4 - c.Radio              // decision parameter
+
+	for point.X <= point.Y {
+		coordinates = append(coordinates, Coordinate{
+			X: point.X,
+			Y: point.Y,
+		})
+
+		if p < 0 {
+			point.X++
+			p = p + 2*point.X + 1
+		} else {
+			point.X++
+			point.Y--
+			p = p + 2*point.X + 1 - 2*point.Y
+		}
+	}
 
 	return coordinates
 }
--- domain/circle_test.go
+++ domain/circle_test.go
@@ -5,17 +5,31 @@ import (
 	"testing"
 )
 
-func TestCircle_GetCoordinates(t *testing.T) {
+func TestCircle_getInitialCoordinates(t *testing.T) {
 	tests := []struct {
 		name string
 		c    *Circle
 		want Coordinates
 	}{
-		// TODO: add test cases
+		{
+			name: "center (2, 3) and radio 10",
+			c:    NewCircle(Coordinate{2, 3}, 10),
+			want: Coordinates{
+				Coordinate{0, 10},
+				Coordinate{1, 10},
+				Coordinate{2, 10},
+				Coordinate{3, 10},
+				Coordinate{4, 9},
+				Coordinate{5, 9},
+				Coordinate{6, 8},
+				Coordinate{7, 7},
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.c.GetCoordinates(); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.c.getInitialCoordinates(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("expectd: %v, got: %v", tt.want, got)
 			}
 		})
